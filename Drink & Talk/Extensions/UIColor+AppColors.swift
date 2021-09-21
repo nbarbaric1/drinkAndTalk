789eb137daf286236cb5b2e5e7077ea952ba2661
Nikola <barbarinikola3@gmail.com>
2021-09-21 19:31:07 +0200

    initial commit

---
+++ Drink & Talk/Extensions/UIColor+AppColors.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+AppColors.swift
+//  Drink & Talk
+//
+//  Created by Nikola Barbarić on 21.09.2021..
+//
+
+import UIKit
+
+extension UIColor {
+  static let myOrange = #colorLiteral(red: 1, green: 0.6745098039, blue: 0.02745098039, alpha: 1)
+}

33d1d1dc47c4cd05f4fffe5927f77d298ac766d6
Nikola <barbarinikola3@gmail.com>
2021-09-21 19:18:24 +0200

    Initial Commit

---
+++ Drink & Talk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Drink & Talk
+//
+//  Created by Nikola Barbarić on 21.09.2021..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
